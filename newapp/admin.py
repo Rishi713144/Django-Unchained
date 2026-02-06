46e6d2bcb225c62320e3fdb20320fd4a89fde006
Rishi <116449371+Rishi713144@users.noreply.github.com>
2026-02-06 15:05:37 +0530

    Django Relationship models

--- newapp/admin.py
+++ newapp/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
-from .models import newappmodel
+from .models import newappmodel, Comment, Self, UserProfile
 # Register your models here.
-admin.site.register(newappmodel)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin.register(newappmodel)
+class newappmodelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    search_fields = ('name', 'type')
+    inlines = [CommentInline]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('newappmodel', 'User', 'rating', 'created_at')
+    search_fields = ('newappmodel__name', 'User__username')
+
+@admin.register(Self)
+class SelfAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    search_fields = ('name', 'location')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+    search_fields = ('user__username', 'bio')
--- newapp/models.py
+++ newapp/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 class newappmodel(models.Model):
     NEWAPP_DB_CHOICES=[
-        ('ML','New Machine'),
-        ('AI','New Machine'),
+        ('ML','Machine-1'),
+        ('AI','Machine-2'),
     ]
     name = models.CharField(max_length=100)
     image=models.ImageField(upload_to='images/')
@@ -12,10 +13,31 @@ class newappmodel(models.Model):
     type=models.CharField(max_length=2,choices=NEWAPP_DB_CHOICES)
     description=models.TextField(default='')
 
-
-
-
-
-
     def __str__(self):
         return self.name
+
+class Comment(models.Model):
+    newappmodel = models.ForeignKey(newappmodel, on_delete=models.CASCADE, related_name='comments')
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+    comment_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.User.username} on {self.newappmodel.name}'
+
+class Self(models.Model):
+    name=models.CharField(max_length=100)
+    location=models.CharField(max_length=100)
+    app_varieties=models.ManyToManyField(newappmodel, related_name='shelves')
+    def __str__(self):
+        return self.name
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+
+    def __str__(self):
+        return self.user.username
+
